97cdd54d15aeb099dc9eb7909eb60c21cfe4d050
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2008-11-13 22:09:04 +0000

    Added support for SocialCalc tabs

--- README.txt
+++ README.txt
@@ -5,12 +5,9 @@ INSTALLATION
 You need the SocialCalc JavaScript spreadsheet first.
 
 To get the latest SocialCalc code, checkout from the SVN repo:
-$ svn co https://repo.socialtext.net:8999/svn/socialcalc/trunk
-
-Rename trunk to socialcalc.
-$ mv trunk socialcalc
+$ svn co https://repo.socialtext.net:8999/svn/socialcalc/trunk socialcalc
 
 Apply the patch inside the socialcalc folder:
 $ cd socialcalc
-$ patch -p0 < socialcalctableeditor-absolute.patch
+$ patch -p0 < ../socialcalctableeditor-absolute.patch
 
--- sheetnode.js
+++ sheetnode.js
@@ -2,13 +2,44 @@
 
 Drupal.sheetnode = {};
 
+Drupal.sheetnode.onclicks = [];
+Drupal.sheetnode.onunclicks = [];
+
+Drupal.sheetnode.absoluteShowHide = function(tab) {
+  this.onclicks[this.sheet.tabs[tab].name] = this.sheet.tabs[tab].onclick;
+  this.sheet.tabs[tab].onclick = function(s, t) {
+    if (Drupal.sheetnode.onclicks[t]) {
+      Drupal.sheetnode.onclicks[t](s, t);
+    }
+    $(".absolute").show();
+  }
+  this.onunclicks[this.sheet.tabs[tab].name] = this.sheet.tabs[tab].onunclick;
+  this.sheet.tabs[tab].onunclick = function(s, t) {
+    if (Drupal.sheetnode.onunclicks[t]) {
+      Drupal.sheetnode.onunclicks[t](s, t);
+    }
+    $(".absolute").hide();
+  }
+}
+
 Drupal.sheetnode.startUp = function() {
   SocialCalc.Constants.defaultImagePrefix = Drupal.settings.sheetnode.imageprefix;
+  SocialCalc.Constants.defaultCommentStyle = "background-repeat:no-repeat;background-position:top right;background-image:url("+ Drupal.settings.sheetnode.imageprefix +"-commentbg.gif);"
+  SocialCalc.Constants.TCendcapClass = 
+  SocialCalc.Constants.TCpanesliderClass = 
+  SocialCalc.Constants.TClessbuttonClass = 
+  SocialCalc.Constants.TCmorebuttonClass = 
+  SocialCalc.Constants.TCscrollareaClass = 
+  SocialCalc.Constants.TCthumbClass = "absolute";
+
   this.sheet = new SocialCalc.SpreadsheetControl();
-  this.sheet.tabbackground="display:none;";
-  this.sheet.toolbarbackground="display:none;";
+  Drupal.sheetnode.absoluteShowHide(this.sheet.tabnums.edit);
+  Drupal.sheetnode.absoluteShowHide(this.sheet.tabnums.sort);
+  Drupal.sheetnode.absoluteShowHide(this.sheet.tabnums.comment);
+  Drupal.sheetnode.absoluteShowHide(this.sheet.tabnums.names);
   this.sheet.ParseSheetSave(Drupal.settings.sheetnode.value);
   this.sheet.InitializeSpreadsheetControl(Drupal.settings.sheetnode.element);
+  this.sheet.currentTab = this.sheet.tabnums.edit;
 }
 
 Drupal.sheetnode.resize = function() {
